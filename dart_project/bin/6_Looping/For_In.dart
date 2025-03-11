32c6cb89f426f174d2f4d0b73f66f30ab6a09959
BagasRozzaqFadli <BagaroFa337@gmail.com>
2025-03-11 08:24:45 +0700

    6_Looping

---
+++ dart_project/bin/6_Looping/For_In.dart
@@ -0,0 +1,11 @@
+void main() {
+  List<String> listnama = ['Bagas', 'Rozzaq', 'Fadli'];
+  for (String nama in listnama) {
+    print(nama);
+  }
+
+  List<int> listAngka = [2, 2, 1, 2];
+  for (int angka in listAngka) {
+    print(angka);
+  }
+}
---
+++ dart_project/bin/6_Looping/While.dart
@@ -0,0 +1,12 @@
+void main() {
+  //untuk increment
+  for (var angka = 1; angka <= 10; angka++) {
+    print('angka $angka');
+  }
+
+  print('');
+//untuk decrement
+  for (var angka = 10; angka >= 1; angka--) {
+    print('angka $angka');
+  }
+}
